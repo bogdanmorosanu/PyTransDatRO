97bf8fa3d4a48a6f07ace5fce7e906be5fb1faf2
Bogdan Morosanu <29919929+bogdanmorosanu@users.noreply.github.com>
2021-02-24 17:43:16 +0000

    Update pyTransDat.py
    Added StereoOblProj  class implementation
    Changed scope of ConfSph class  - inner class of StereoOblProj

--- pyTransDat.py
+++ pyTransDat.py
@@ -75,70 +75,53 @@ class StereoOblProj():
         :type s: float    
         """
 
-        self.__ell = ell
-        self.__orig_lat = orig_lat
-        self.__orig_lon = orig_lon
-        self.__f_n = f_n
-        self.__f_e = f_e
-        self.__s = s
+        self._ell = ell
+        self._orig_lat = orig_lat
+        self._orig_lon = orig_lon
+        self._f_n = f_n
+        self._f_e = f_e
+        self._s = s
+        self.__conf_sphere = self.ConfSph(self)
 
-
-class ConfSph():
-    """
-    Class defining a conformal sphere
-    """
-    def __init__(self, ell, proj_orig_lat, proj_orig_lon,
-                 proj_f_n, proj_f_e, proj_s):
+    def to_grid(self, lat, lon):
         """
-        :param ell: projection's ellipsoid
-        :type ell: Ellipsoid
+        Computes the grid coordinates (projected) of the input geodetic
+        (geographic) coordinates (Lat, Long) -> (N, E)
 
-        :param proj_orig_lat: projection's geodetic origin latitude
-        :type proj_orig_lat: float
+        :param lat: latitude
+        :type lat: float
 
-        :param proj_orig_lon: projection's geodetic origin longitude
-        :type proj_orig_lon: float 
+        :param lon: longitude
+        :type lon: float 
 
-        :param f_n: projection's false northing
-        :type f_n: float
-
-        :param f_e: projection's false easting
-        :type f_e: float
-
-        :param s: projection's scale factor
-        :type s: float   
+        :return: The values of northing and easting
+        :rtype: tuple of floats           
         """
+        lat_c, lon_c = self.__conf_sphere.get_latlon_from_geo(lat, lon)
+        lat_c0 = self.__conf_sphere.orig_lat
+        lon_c0 = self.__conf_sphere.orig_lon
 
-        # calculate parameters defining the conformal sphere (tagged CSP)
-        ## some vars used in calculus (precompute values to optimeze and 
-        ## improve readability)
-        r_m, r_n = ell.get_rad_M_N(proj_orig_lat)
-        e2 = ell.first_ecc2
-        e = ell.first_ecc
-        sin_orig_lat = math.sin(proj_orig_lat)
-        s1 = (1 + sin_orig_lat) / (1 - sin_orig_lat)
-        s2 = (1 - e * sin_orig_lat) / (1 + e * sin_orig_lat)
-        r = math.sqrt(r_m * r_n)   # CSP
-        n = math.sqrt(1 + (e2 * math.pow(math.cos(proj_orig_lat), 4) / (1 - e2)))   # CSP
-        w1 = math.pow(s1, n) * math.pow(s2, e * n)
-        sin_chi0 = (w1 - 1) / (w1 + 1)
-        c = (n + sin_orig_lat) * (1 - sin_chi0) / ((n - sin_orig_lat) * (1 + sin_chi0))   # CSP
-        w2 = c * w1
+        # some vars used in calculus (precompute values to optimize computation
+        # and improve readability)        
+        sin_lat_c = math.sin(lat_c)
+        cos_lat_c = math.cos(lat_c)
+        sin_lat_c0 = math.sin(lat_c0)
+        cos_lat_c0 = math.cos(lat_c0)
+        dlon = lon_c - lon_c0
+        b = (1 + sin_lat_c * sin_lat_c0 
+             + cos_lat_c * cos_lat_c0 * math.cos(dlon))
         
-        self.__f_n = proj_f_n
-        self.__f_e = proj_f_e
-        self.__s = proj_s
-        self.__ell_first_ecc = ell.first_ecc
-        self.__r = r
-        self.__n = n
-        self.__c = c
-        self.__orig_lat = math.asin((w2 - 1) / (w2 + 1))
-        self.__orig_lon = proj_orig_lon
+        return ((self._f_e + 2 * self.__conf_sphere.r 
+                               * self._s * cos_lat_c * math.sin(dlon) / b),
+                (self._f_n + 2 * self.__conf_sphere.r * self._s 
+                               * (sin_lat_c * cos_lat_c0 
+                                  - cos_lat_c * sin_lat_c0 * math.cos(dlon)) 
+                                / b))
 
-    def get_latlon_from_grid(self, n, e):
+    def to_geo(self, n, e):
         """
-        Computes the equivalent conformal latitude and longitude for a given 
-        point with Stereographic grid coordinates (N,E)
+        Computes the geodetic (geographic) coordinates of the input grid
+        (projected) coordinates (N, E) -> (Lat, Long)
 
         :param n: northing
         :type n: float
@@ -146,47 +129,132 @@ class ConfSph():
         :param e: easting
         :type e: float 
 
-        :return: The values of Latitude and Longitude
-        :rtype: tuple of floats               
+        :return: The values of latitude and longitude
+        :rtype: tuple of floats           
         """
 
-        # some vars used in calculus (precompute values to optimeze and 
-        # improve readability)
-        g = 2 * self.__r * self.__s * math.tan((math.pi / 4) - (self.__orig_lat / 2))
-        h = 4 * self.__r * self.__s * math.tan(self.__orig_lat) + g
-        dn = n - self.__f_n
-        de = e - self.__f_e
-        i = math.atan(de / (h + dn))
-        j = (math.atan(de / (g - dn))) - i  
+        lat_c, lon_c = self.__conf_sphere.get_latlon_from_grid(n, e)
 
-        return (self.__orig_lat + 2 * math.atan((dn - de * math.tan(j / 2))
-                                                / (2 * self.__r * self.__s)), 
-                j + 2 * i + self.__orig_lon)
+        iso_lat = ((0.5 * math.log((1 + math.sin(lat_c))
+                   / (self.__conf_sphere.c * (1 - math.sin(lat_c))))) 
+                   / self.__conf_sphere.n)
+        e = self._ell.first_ecc
+        e2 = self._ell.first_ecc2
 
-    def get_latlon_from_geo(self, lat, lon):
+        # next computation involves an iterative process until 
+        # precision is achieved (tolerance and counter used to control the loop)
+        tol = 0.0000000000484814   # radians of DMS value: 0 0 0.00001
+        i = 0   # counter to avoid infinite loop
+        diff = tol + 1
+        r_lat = 0.0   # assign a default value for result latitude
+        while diff >= tol and i < 50:
+            if i == 0:   # if first iteration compute first approximation
+                r_lat = 2 * math.atan(math.exp(iso_lat)) - math.pi / 2
+            else:
+                r_lat_before_next_iter = r_lat
+                iso_lat_i = math.log(math.tan(r_lat / 2 + math.pi / 4)
+                                     * math.pow((1 - e * math.sin(r_lat))
+                                     / (1 + e * math.sin(r_lat)), e / 2))
+
+                r_lat = r_lat - ((iso_lat_i - iso_lat) * math.cos(r_lat)
+                                 * (1 - e2 * math.pow(math.sin(r_lat), 2)) 
+                                 / (1 - e2))
+                diff = abs(r_lat_before_next_iter - r_lat)
+            i+=1
+        
+        return (r_lat,
+                self._orig_lon + (lon_c - self._orig_lon) / self.__conf_sphere.n)
+
+        
+    class ConfSph():
         """
-        Computes the equivalent conformal latitude and longitude for a given
-        point with geodetic geographic coordinates (latitude, longitude)
-              
-        :param lat: latitude
-        :type lat: float
-
-        :param lon: longitude
-        :type lon: float 
-
-        :return: The values of Latitude and Longitude
-        :rtype: tuple of floats         
+        Class defining a conformal sphere
         """
+        def __init__(self, proj):
+            """
+            :param proj: the projection associated with the conformal sphere
+            :type proj: StereoOblProj 
+            """
 
-        # some vars used in calculus (precompute values to optimeze and 
-        # improve readability)
-        sin_lat = math.sin(lat)
-        sa = (1 + sin_lat) / (1 - sin_lat)
-        sb = (1 - self.__ell_first_ecc * sin_lat) / (1 + self.__ell_first_ecc * sin_lat)
-        w = self.__c * math.pow((sa * math.pow(sb, self.__ell_first_ecc)), self.__n)
+            # calculate parameters defining the conformal sphere (tagged CSP)
+            ## some vars used in calculus (precompute values to optimize
+            ## computation and improve readability)
+            r_m, r_n = proj._ell.get_rad_M_N(proj._orig_lat)
+            e2 = proj._ell.first_ecc2
+            e = proj._ell.first_ecc
+            sin_orig_lat = math.sin(proj._orig_lat)
+            s1 = (1 + sin_orig_lat) / (1 - sin_orig_lat)
+            s2 = (1 - e * sin_orig_lat) / (1 + e * sin_orig_lat)
+            r = math.sqrt(r_m * r_n)   # CSP
+            n = math.sqrt(1 + (e2 * math.pow(math.cos(proj._orig_lat), 4) 
+                               / (1 - e2)))   # CSP
+            w1 = math.pow(s1, n) * math.pow(s2, e * n)
+            sin_chi0 = (w1 - 1) / (w1 + 1)
+            c = (n + sin_orig_lat) * (1 - sin_chi0) / ((n - sin_orig_lat) 
+                                                       * (1 + sin_chi0))   # CSP
+            w2 = c * w1
+            
+            self.__proj = proj
+            self.r = r
+            self.n = n
+            self.c = c
+            self.orig_lat = math.asin((w2 - 1) / (w2 + 1))
+            self.orig_lon = proj._orig_lon
 
-        return (self.__n * (lat - self.__orig_lon) + self.__orig_lon,
-                math.asin((w - 1) / (w + 1)))       
+        def get_latlon_from_grid(self, n, e):
+            """
+            Computes the equivalent conformal latitude and longitude for a given 
+            point with Stereographic grid coordinates (N,E)
+
+            :param n: northing
+            :type n: float
+
+            :param e: easting
+            :type e: float 
+
+            :return: The values of Latitude and Longitude
+            :rtype: tuple of floats               
+            """
+
+            # some vars used in calculus (precompute values to optimize 
+            # computation and improve readability) 
+            sf = self.__proj._s   # projection scale factor
+            g = 2 * self.r * sf * math.tan((math.pi / 4) - (self.orig_lat / 2))
+            h = 4 * self.r * sf * math.tan(self.orig_lat) + g
+            dn = n - self.__proj._f_n
+            de = e - self.__proj._f_e
+            i = math.atan(de / (h + dn))
+            j = (math.atan(de / (g - dn))) - i  
+
+            return (self.orig_lat + 2 * math.atan((dn - de * math.tan(j / 2))
+                                                    / (2 * self.r * sf)), 
+                    j + 2 * i + self.orig_lon)
+
+        def get_latlon_from_geo(self, lat, lon):
+            """
+            Computes the equivalent conformal latitude and longitude for a given
+            point with geodetic geographic coordinates (latitude, longitude)
+                
+            :param lat: latitude
+            :type lat: float
+
+            :param lon: longitude
+            :type lon: float 
+
+            :return: The values of Latitude and Longitude
+            :rtype: tuple of floats         
+            """
+
+            # some vars used in calculus (precompute values to optimize 
+            # computation and improve readability)
+            e = self.__proj._ell.first_ecc
+            sin_lat = math.sin(lat)
+            sa = (1 + sin_lat) / (1 - sin_lat)
+            sb = (1 - e * sin_lat) / (1 + e * sin_lat)
+            w = self.c * math.pow((sa * math.pow(sb, e)), self.n)
+
+            return (self.n * (lat - self.orig_lon) + self.orig_lon,
+                    math.asin((w - 1) / (w + 1)))       
 
 
 class Helmert2D():
