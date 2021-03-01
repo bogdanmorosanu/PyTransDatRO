e294b9d2ddab93df6e9c3c7c45408681bae2634a
Bogdan Morosanu <29919929+bogdanmorosanu@users.noreply.github.com>
2021-03-01 17:34:05 +0000

    Update trans_helmert2d.py
    Code for reversed case reorganised

--- trans_helmert2d.py
+++ trans_helmert2d.py
@@ -8,10 +8,10 @@ class Helmert2D():
     """
 
     # constants to define the Stereo70 to Stereo on GRS80 Helmert transformation
-    _T_N = -31.8051   # translation on North
-    _T_E = -119.7358   # translation on East
-    _SCALE = -0.11559991   # ppm scale factor
-    _ROTATION = '0 0 0.22739706'   # latitude of natural origin
+    T_N = -31.8051   # translation on North
+    T_E = -119.7358   # translation on East
+    PPM_SCALE = -0.11559991   # ppm scale factor
+    ROTATION = '0 0 0.22739706'   # latitude of natural origin
     
 
     def __init__(self, reversed):
@@ -19,12 +19,17 @@ class Helmert2D():
         :param reversed: initialize the reversed transformation
         :type reversed: bool 
         """
-        self.__t_e = Helmert2D._T_E if not reversed else -Helmert2D._T_E
-        self.__t_n = Helmert2D._T_N if not reversed else -Helmert2D._T_N
-        scale_unity = ppm_to_unity(Helmert2D._SCALE)
-        tmp_s = scale_unity if not reversed else 2 - scale_unity
-        rotation_rads = sexa_to_rad(Helmert2D._ROTATION)
-        tmp_r = rotation_rads if not reversed else - rotation_rads
+        if reversed:
+            self.__t_e = -Helmert2D.T_E
+            self.__t_n = -Helmert2D.T_N
+            tmp_s = 2 - ppm_to_unity(Helmert2D.PPM_SCALE)
+            tmp_r = - sexa_to_rad(Helmert2D.ROTATION)
+        else:
+            self.__t_e = Helmert2D.T_E 
+            self.__t_n = Helmert2D.T_N 
+            tmp_s = ppm_to_unity(Helmert2D.PPM_SCALE)
+            tmp_r = sexa_to_rad(Helmert2D.ROTATION)
+
         self.__sxsinr = tmp_s * math.sin(tmp_r)   # s * sin(r)
         self.__sxcosr = tmp_s * math.cos(tmp_r)   # s * cos(r)
     
@@ -41,6 +46,6 @@ class Helmert2D():
 
         :return: transformed x' and y' values
         :rtype: tuple of floats                 
-        """
+        """       
         return (n * self.__sxcosr + e * self.__sxsinr + self.__t_n,
                 e * self.__sxcosr - n * self.__sxsinr + self.__t_e)
