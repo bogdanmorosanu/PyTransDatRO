7e37075ed33d0f0486c8d259d180e7b0a11a2e5b
Bogdan Morosanu <29919929+bogdanmorosanu@users.noreply.github.com>
2021-03-01 17:37:06 +0000

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,11 +1,12 @@
-    """Defines expections that are thrown be pyTransDat
-    """
+"""Defines expections that are thrown be pyTransDat
+"""
 class OutOfGridErr(Exception):
     """Signifies that the input location (N,E) 
-    is not cover by a grid / is out of a grid's min/maxt
+    is not cover by a grid / is out of a grid's min/max
     """
     def __init__(self, n, e, grid):
-        """
+        """Constructor method
+
         :param n: northing
         :type n: float
 
@@ -13,13 +14,13 @@ class OutOfGridErr(Exception):
         :type e: float 
 
         :param grid: Grid against which values (n, e) have been tested
-        :type grid: Grid
+        :type grid: Grid        
         """
         message = (
             f'({n}, {e}) location is out of the grid area.'
             f'Location must be inside the grid defined by '
-            f'Min N = {n_min}, Min E = {e_min}, '
-            f'Max N = {n_max}, Max E  = {e_max}.'
+            f'Min N = {grid.n_min}, Min E = {grid.e_min}, '
+            f'Max N = {grid.n_max}, Max E  = {grid.e_max}.'
         )
         super().__init__(message)
 
@@ -29,7 +30,8 @@ class NoDataGridErr(Exception):
     therefore computation is not possible
     """
     def __init__(self, n, e, grid):
-        """
+        """Constructor method
+
         :param n: northing
         :type n: float
 
@@ -43,4 +45,5 @@ class NoDataGridErr(Exception):
             f'(The subgrid of size {grid.sg_size}x{grid.sg_size} required '
             f'for the computation of interpolated value at {n}, {e} location '
             f'has No Data value(s).'
-        )        
+        )
+        super().__init__(message)        
