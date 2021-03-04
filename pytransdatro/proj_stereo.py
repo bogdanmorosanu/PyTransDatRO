095781a2d0c0bb3c6e8d57a1f84efa21e25be653
Bogdan Morosanu <29919929+bogdanmorosanu@users.noreply.github.com>
2021-03-04 22:47:47 +0000

    Update proj_stereo.py
    Final touch on the docstring and comments

--- pytransdatro/proj_stereo.py
+++ pytransdatro/proj_stereo.py
@@ -1,16 +1,42 @@
+"""This module stores classes for the coordinate conversion of the 
+Oblique Stereographic projection used by PyTransDatRO. The projection's 
+parameters values were extracted from the document 
+Help_TransDatRO_code_source_EN.pdf and are assigned when a StereoProj object is
+instantiated 
+
+Usage:
+The purpose of StereoProj is to be part of the Stere70 <-> ETRS89 transformation
+defined in the module trans_ro. There should be no purpose of using it in
+another context.
+Code example:
+    st_proj = StereoProj()
+    lat, lon = st_proj.to_geo(n, e)   # (Lat, Long) -> (N, E)
+    n, e = st_proj.to_grid(lat, lon)  # (N, E) -> (Lat, Long) 
+
+Notes:
+    - geographic cooordinates (lat, lon) are expressed in radians
+    - grid (projected) coordinates (n, e) are expressed in meters
+
+Classes:
+    - StereoProj
+        - _Ellipsoid
+        - _ConfSph
+ 
+"""
 import math
 from pytransdatro.utils import sexa_to_rad
 
 class StereoProj():
-    """
-    Class which provides functions for Oblique Stereographic projection
+    """Class which provides functions for Oblique Stereographic projection
     coordinate conversion.
     (Lat, Long) -> (N, E)
     (N, E) -> (Lat, Long)
     """
-    # constants to define the WGS84 ellipsoid used by this projection
+    # constants which define the WGS84 ellipsoid used by this projection
     WGS84_ELL_SMAJ_AXIS = 6378137   # ellipsoid's semi-major axis 
     WGS84_ELL_INV_FLAT = 298.257223563   # ellipsoid's inverse flattening
+    
+    # constants which define the projection parameters
     ORIG_LAT = '46 0 0.0'   # latitude of natural origin
     ORIG_LON = '25 0 0.0'   # longitude of natural origin
     FALSE_N = 500000   # false northing
@@ -18,9 +44,8 @@ class StereoProj():
     PROJ_SCALE = 0.99975   # scale factor
 
     def __init__(self):
-        """Contructor method 
+        """Contructor method which sets the parameters of this projection.
         """
-
         self._ell = self._Ellipsoid(StereoProj.WGS84_ELL_SMAJ_AXIS,
                                     StereoProj.WGS84_ELL_INV_FLAT)
         self._orig_lat = sexa_to_rad(StereoProj.ORIG_LAT)
@@ -31,8 +56,7 @@ class StereoProj():
         self.__conf_sphere = self._ConfSph(self)
 
     def to_grid(self, lat, lon):
-        """
-        Returns the grid coordinates (projected) of the input geodetic
+        """Returns the grid coordinates (projected) of the input geodetic
         (geographic) coordinates 
         (Lat, Long) -> (N, E)
 
@@ -42,7 +66,7 @@ class StereoProj():
         :param lon: longitude
         :type lon: float 
 
-        :return: The values of northing and easting
+        :return: The values of northing and easting (n,e)
         :rtype: tuple of floats           
         """
         lat_c, lon_c = self.__conf_sphere.get_latlon_from_geo(lat, lon)
@@ -67,8 +91,7 @@ class StereoProj():
                                * self._s * cos_lat_c * math.sin(dlon) / b))
 
     def to_geo(self, n, e):
-        """
-        Returns the geodetic (geographic) coordinates of the input grid
+        """Returns the geodetic (geographic) coordinates of the input grid
         (projected) coordinates 
         (N, E) -> (Lat, Long)
 
@@ -78,7 +101,7 @@ class StereoProj():
         :param e: easting
         :type e: float 
 
-        :return: The values of latitude and longitude
+        :return: The values of latitude and longitude  (lat,lon)
         :rtype: tuple of floats           
         """
 
@@ -115,16 +138,15 @@ class StereoProj():
                 self._orig_lon + (lon_c - self._orig_lon) / self.__conf_sphere.n)
 
     class _Ellipsoid:
-        """
-        Class which provides basic ellipsoid element computation.
-
+        """Class which provides functionality for computation of basic 
+        ellipsoid's elements.
         """
         def __init__(self, smaj_axis, inv_flat):
             """
             :param smaj_axis: semi-major axis of the ellipsoid
             :type smaj_axis: float
 
-            :param inv_flat: inverse flattening of the ellipsoid (1 / flattening)
+            :param inv_flat: inverse flattening of the ellipsoid (1/flattening)
             :type inv_flat: float
 
             :ivar flat: flattening
@@ -144,27 +166,27 @@ class StereoProj():
             self.sec_ecc = math.sqrt(self.sec_ecc2)
         
         def get_rad_M_N(self, lat):
-            """
-            Calculates the radius of curvature in meridian - north-south direction (M)
-            and the radius of curvature in prime vertical - east-west direction (N)
+            """Calculates the radius of curvature in meridian - north-south 
+            direction (M) and the radius of curvature in prime vertical - 
+            east-west direction (N)
 
             :param lat: the latitude for which M and N values are computed
             :type lat: float
 
-            :return: The values of M and N
+            :return: The values of M and N - (M,N)
             :rtype: tuple of floats
             """
             # temporary value used for computation of both M and N
-            # sqrt(1 - e^2 * sin^2(lat)), where e - first_ecc2
+            # sqrt(1 - e^2 * sin^2(lat)), where e = first_ecc
             tmp_v = math.sqrt((1 - self.first_ecc2 * math.pow(math.sin(lat), 2)))
             m = self.smaj_axis * (1 - self.first_ecc2) / math.pow(tmp_v, 3)
             n = self.smaj_axis / tmp_v
             return m, n
 
-
     class _ConfSph():
-        """
-        Class defining a conformal sphere
+        """Class defining a conformal sphere.
+        Provides methods to compute the equivalent conformal latitude and 
+        longitude of a given point
         """
         def __init__(self, proj):
             """
@@ -198,9 +220,8 @@ class StereoProj():
             self.orig_lon = proj._orig_lon
 
         def get_latlon_from_grid(self, n, e):
-            """
-            Computes the equivalent conformal latitude and longitude for a given 
-            point with Stereographic grid coordinates (N,E)
+            """Computes the equivalent conformal latitude and longitude for a 
+            given point with stereographic grid coordinates (N,E)
 
             :param n: northing
             :type n: float
@@ -208,7 +229,7 @@ class StereoProj():
             :param e: easting
             :type e: float 
 
-            :return: The values of Latitude and Longitude
+            :return: the values of latitude and longitude (lat,lon)
             :rtype: tuple of floats               
             """
 
@@ -227,17 +248,17 @@ class StereoProj():
                     j + 2 * i + self.orig_lon)
 
         def get_latlon_from_geo(self, lat, lon):
-            """
-            Computes the equivalent conformal latitude and longitude for a given
-            point with geodetic geographic coordinates (latitude, longitude)
+            """Computes the equivalent conformal latitude and longitude for a 
+            given point with geodetic geographic coordinates (latitude, 
+            longitude)
                 
-            :param lat: latitude
+            :param lat: latitude in radians
             :type lat: float
 
-            :param lon: longitude
+            :param lon: longitude in radians
             :type lon: float 
 
-            :return: The values of Latitude and Longitude
+            :return: the values of latitude and longitude (lat,lon)
             :rtype: tuple of floats         
             """
 
